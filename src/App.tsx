51ff16a405c421818bec7d1fb09391d1224d78e2
moskalyk <morgan.moskalyk@protonmail.ch>
2024-02-28 12:59:16 -0500

    updated project

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,33 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
   const [address, setAddress] = useState<any>(null)
 
-  sequence.initWallet('', {defaultNetwork: 'polygon'})
+  sequence.initWallet('AQAAAAAAABT5pgun7vPWSs8wsI4BzaQJmlI', {defaultNetwork: 'polygon'})
 
   const connect = async () => {
-    const wallet = sequence.getWallet()
     try {
+      let wallet = sequence.getWallet()
+      console.log('already connected')
+      console.log(wallet.getAddress())
+      setIsLoggedIn(true)
+      setAddress(wallet.getAddress())
+    } catch(err){
+      console.log('not connected')
+      let wallet = sequence.getWallet()
       const details = await wallet.connect({app: 'demo-vite-starter'})
       if(details.connected){
         setIsLoggedIn(true)
         setAddress(details.session?.accountAddress)
       }
-    }catch(err){
-      console.log(err)
     }
+  }
+  const disconnect = () => {
+    localStorage.clear()
   }
 
   return (
     <>
         {!isLoggedIn ? <button onClick={() => connect()}>connect</button> : <>{"connected " + address}</>}
+        {!isLoggedIn ? <button onClick={() => disconnect()}>disconnect</button> : <>{"connected " + address}</>}
     </>
   )
 }
